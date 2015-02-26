11dab5dfc3531489e1b973f1080fb176a8950a2d
inCodeSpace <box4goodmail@gmail.com>
2015-02-26 14:59:56 +0300

    add2

--- _protected/config/urlManager.php
+++ _protected/config/urlManager.php
@@ -1,19 +1,10 @@
 <?php
-
-<<<<<<< HEAD
 return [
 /*
-=======
-return [/*
->>>>>>> origin/master
     'enablePrettyUrl' => true,
     'showScriptName' => false,
     // 'enableStrictParsing' => true,
     'rules' => [
     ],
-<<<<<<< HEAD
 */
 ];
-=======
-*/];
->>>>>>> origin/master
